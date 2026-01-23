5477a09b9892f635acde2c54c9d7798ddc544826
Nicolas <nicolascontatotrabalho@gmail.com>
2026-01-23 19:46:37 -0300

    Project created

---
+++ src/test/java/com/nicolas/course/CourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nicolas.course;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
